96ba567353c290bd38a0406227b4879175b03df9
chamokBarmon <chamokbarmon123@gmail.com>
2022-11-10 20:41:27 +0600

    'last'

--- index.js
+++ index.js
@@ -15,12 +15,18 @@ async function run(){
     try{
        const serviceCollection =client.db('services').collection('userdata')
        app.get('/services', async(req,res)=>{
-          const query ={};
+         
           const cursor = serviceCollection.find(query)
           const services = await cursor.toArray();
           res.send(services)
        });
        app.get('/services/:id', async(req,res)=>{
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)}
+          const service = await serviceCollection.findOne(query);
+          res.send(service)
+       })
+       app.get('/review/:id', async(req,res)=>{
           const id = req.params.id;
           const query = {_id: ObjectId(id)}
           const service = await serviceCollection.findOne(query);
